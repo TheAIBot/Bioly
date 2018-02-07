beca620c92c735d74c4ccea8bf10afc56036f114
TheAIBot <andreasuranus@hotmail.com>
2018-02-07 10:40:10 +0100

    added blockly webpage

--- BiolyViewer-Windows/MainWindow.xaml.cs
+++ BiolyViewer-Windows/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace BiolyViewer_Windows
             settings.RegisterScheme(new CefCustomScheme
             {
                 SchemeName = "costum",
-                SchemeHandlerFactory = new FolderSchemeHandlerFactory(rootFolder: @"webpage"),
+                SchemeHandlerFactory = new FolderSchemeHandlerFactory(rootFolder: @"../../../webpage"),
                 IsSecure = true
             });
 
