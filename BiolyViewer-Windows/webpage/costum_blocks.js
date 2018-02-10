cf138d2e28ec2bc7d193cc20ee1f2388cc5f7674
TheAIBot <andreasuranus@hotmail.com>
2018-02-10 17:00:42 +0100

    initial blockly programming blocks

--- BiolyViewer-Windows/webpage/costum_blocks.js
+++ BiolyViewer-Windows/webpage/costum_blocks.js
@@ -1,25 +1,279 @@
-Blockly.Blocks['mix'] = {
-  init: function() {
-    this.jsonInit({
-      "message0": "mix %1 and %2",
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "VALUE",
-          "check": "String"
-        },
-		{
-          "type": "input_value",
-          "name": "VALUddE",
-          "check": "String"
-        }
-      ],
-	  "previousStatement": null,
-	  "nextStatement": null,
-      "output": "Number",
-      "colour": 160,
-      "tooltip": "Returns number of letters in the provided text.",
-      "helpUrl": "http://www.w3schools.com/jsref/jsref_length_string.asp"
-    });
-  }
+Blockly.Blocks["start"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "start %1",
+			"args0": [
+				{
+					"type": "input_statement",
+					"name": "program"
+				}
+			],
+			"colour": 0,
+			"tooltip": "Starts a bio program"
+		});
+	}
 };
+
+Blockly.Blocks["input"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "new input",
+			"args0": [
+			],
+			"message1": "name %1",
+			"args1": [
+				{
+					"type": "field_variable",
+					"name": "inputName",
+					"variable": "input_fluid_name"
+				}
+			],
+			"message2": "amount %1 %2",
+			"args2": [
+				{
+					"type": "field_input",
+					"name": "inputAmount",
+					"check": "Number"
+				},
+				{
+					"type": "field_dropdown",
+					"name": "inputUnit",
+					"options": [
+						["drops", "drops"],
+						["ml", "ml"]
+					]
+				}
+			],
+			"previousStatement": null,
+			"nextStatement": null,
+			"colour": 40,
+			"tooltip": ""
+		});
+	}
+};
+Blockly.Blocks["getInput"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "input %1",
+			"args0": [
+				{
+					"type": "field_variable",
+					"name": "inputName",
+					"variable": "input_fluid_name"
+				}
+			],
+			"output": "InputType",
+			"colour": 40,
+			"tooltip": ""
+		});
+	}
+};
+
+Blockly.Blocks["fluid"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "new fluid",
+			"args0": [
+			],
+			"message1": "name %1 %2",
+			"args1": [
+				{
+					"type": "field_variable",
+					"name": "fluidName",
+					"variable": "fluid_name"
+				},
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType", "FluidOperator"]
+				}
+			],
+			"previousStatement": null,
+			"nextStatement": null,
+			"colour": 80,
+			"tooltip": ""
+		});
+	}
+};
+Blockly.Blocks["getFluid"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "fluid %1",
+			"args0": [
+				{
+					"type": "field_variable",
+					"name": "fluidName",
+					"variable": "fluid_name"
+				}
+			],
+			"output": "FluidType",
+			"colour": 80,
+			"tooltip": ""
+		});
+	}
+};
+
+Blockly.Blocks["mix"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "mix",
+			"args0": [
+			],
+			"message1": "a %1",
+			"args1": [
+				{
+					"type": "input_value",
+					"name": "inputFluidA",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"message2": "b %1",
+			"args2": [
+				{
+					"type": "input_value",
+					"name": "inputFluidB",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"output": "FluidOperator",
+			"colour": 120,
+			"tooltip": ""
+		});
+	}
+};
+
+Blockly.Blocks["split"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "split",
+			"args0": [
+			],
+			"message1": "fluid %1",
+			"args1": [
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"message2": "amount %1",
+			"args2": [
+				{
+					"type": "field_number",
+					"name": "fluidAmount",
+					"check": "Number"
+				}
+			],
+			"output": "FluidOperator",
+			"colour": 160,
+			"tooltip": "",
+			"inputsInline": false
+		});
+	}
+};
+
+Blockly.Blocks["heat"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "heat",
+			"args0": [
+			],
+			"message1": "fluid %1",
+			"args1": [
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"message2": "temperature %1",
+			"args2": [
+				{
+					"type": "field_number",
+					"name": "temperature",
+					"check": "Number"
+				}
+			],
+			"message3": "time %1",
+			"args3": [
+				{
+					"type": "field_number",
+					"name": "time",
+					"check": "Number"
+				}
+			],
+			"output": "FluidOperator",
+			"colour": 200,
+			"tooltip": "",
+			"inputsInline": false
+		});
+	}
+};
+
+Blockly.Blocks["waste"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "waste %1",
+			"args0": [
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"previousStatement": null,
+			"nextStatement": null,
+			"colour": 240,
+			"tooltip": "",
+			"inputsInline": false
+		});
+	}
+};
+
+Blockly.Blocks["output"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "output %1",
+			"args0": [
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"previousStatement": null,
+			"nextStatement": null,
+			"colour": 280,
+			"tooltip": "",
+			"inputsInline": false
+		});
+	}
+};
+
+Blockly.Blocks["sensor"] = {
+	init: function() {
+		this.jsonInit({
+			"message0": "sensor %1",
+			"args0": [
+				{
+					"type": "input_value",
+					"name": "inputFluid",
+					"check": ["InputType", "FluidType"]
+				}
+			],
+			"output": "Number",
+			"colour": 320,
+			"tooltip": "",
+			"inputsInline": false
+		});
+	}
+};
+
+
+
+
+
+
+
+
+
