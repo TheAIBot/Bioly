4e4332f6d64642a1197ffa0fbdf1369191930d60
TheAIBot <andreasuranus@hotmail.com>
2018-02-10 10:32:16 +0100

    crude mixer

--- BiolyViewer-Windows/webpage/costum_blocks.js
+++ BiolyViewer-Windows/webpage/costum_blocks.js
@@ -1,14 +1,21 @@
-Blockly.Blocks['string_length'] = {
+Blockly.Blocks['mix'] = {
   init: function() {
     this.jsonInit({
-      "message0": 'length of %1',
+      "message0": "mix %1 and %2",
       "args0": [
         {
           "type": "input_value",
           "name": "VALUE",
           "check": "String"
+        },
+		{
+          "type": "input_value",
+          "name": "VALUddE",
+          "check": "String"
         }
       ],
+	  "previousStatement": null,
+	  "nextStatement": null,
       "output": "Number",
       "colour": 160,
       "tooltip": "Returns number of letters in the provided text.",
