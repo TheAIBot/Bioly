f2e0baf0587cf6d1b0cc684fbe277d30cb1bccd0
TheAIBot <andreasuranus@hotmail.com>
2018-03-15 05:13:37 +0100

    working on the simulator electrodes

--- webpage/simulator.js
+++ webpage/simulator.js
@@ -2,10 +2,17 @@
 
 var gl;
 var boardVerticies;
+var program;
 
 window.onload = function init()
 {
     const canvas = document.getElementById("simulatorCanvas");
+	const parentWidth = canvas.parentNode.clientWidth;
+	const parentHeight = canvas.parentNode.clientHeight;
+	const canvasSize = Math.min(parentWidth, parentHeight);
+	
+	canvas.width = canvasSize;
+	canvas.height = canvasSize;
     
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) 
@@ -13,54 +20,77 @@ window.onload = function init()
         alert("failed to load webgl");
     }
     
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.viewport(0, 0, canvasSize, canvasSize);
+    gl.clearColor(1, 1, 1, 1.0);
     
     //NOW DRAW SOME SHIT
-    const program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);    
     
 }
 
 function setupSimulator(width, height)
 {
-    var verticies = [
-        vec2(0.0, 0.0),
-        vec2(1.0, 0.0),
-        vec2(1.0, 1.0),
-    ];
+    let verticies = setupBoard(width, height);
     
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(verticies), gl.STATIC_DRAW);
 	
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    const vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 	
-	
+	/*
 	var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(verticies), gl.STATIC_DRAW);
 	
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    
+    const vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+    */
     render(verticies.length);
+	
 }
 
 function setupBoard(width, height)
 {
-	boardVerticies = [];
+	let boardVerticies = [];
+	
+	const borderSize = 0.05;
+	const boardSize = 2 - (borderSize * 2);
+	const topLeftX = (-boardSize / 2);
+	const topLeftY = (boardSize / 2);
+	
+	
+	//ratio between electrode size and electrode spacing
+	const ratioForSpace = 0.1;
+	const electrodeSize =  (boardSize  / Math.max(width, height) ) * (1 - ratioForSpace);
 	
 	for(var y = 0; y < height; y++)
 	{
+		const sumElectrodeHeight = electrodeSize * y;
+		const sumElectrodeHeightSpace = ratioForSpace * y * electrodeSize;
+		const topY = topLeftY - sumElectrodeHeight - sumElectrodeHeightSpace
 		for(var x = 0; x < width; x++)
 		{
+			const sumElectrodeWidth = electrodeSize * x;
+			const sumElectrodeWidthSpace = ratioForSpace * x * electrodeSize;
+			const topX = topLeftX + sumElectrodeWidth + sumElectrodeWidthSpace;
 			
+			const topLeft     = vec2(topX                , topY);
+			const topRight    = vec2(topX + electrodeSize, topY);
+			const bottomLeft  = vec2(topX                , topY + electrodeSize);
+			const bottomRight = vec2(topX + electrodeSize, topY + electrodeSize);
+			
+			//add two triangles that together make the square electrode
+			boardVerticies.push(topLeft   , topRight, bottomLeft);
+			boardVerticies.push(bottomLeft, topRight, bottomRight);
 		}
 	}
+	
+	return boardVerticies;
 }
 
 function render(numPoints) 
