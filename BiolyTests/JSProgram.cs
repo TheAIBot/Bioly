12498b8d70e0e60ce09c6c7aba22bb991fd4b295
TheAIBot <andreasuranus@hotmail.com>
2018-03-28 09:11:46 +0200

    starting to implement segments to the JSProgram class
    segments will fully add a mixer block with everything required to make
    it work correctly.

--- BiolyTests/JSProgram.cs
+++ BiolyTests/JSProgram.cs
@@ -9,6 +9,7 @@ namespace BiolyTests
     public class JSProgram
     {
         StringBuilder Builder = new StringBuilder();
+        int nameID = 0;
 
         public void AddBlock(string name, string blockType)
         {
@@ -18,6 +19,28 @@ namespace BiolyTests
         public void AddConnection(string inputBlockname, string inputName, string outputBlockName)
         {
             Builder.Append($"{inputBlockname}.getInput(\"{inputName}\").connection.connect({outputBlockName}.outputConnection);");
+        }
+
+        public void AddMixerSegment(string outputName, string inputNameA, string inputNameB)
+        {
+            JSProgram program = new JSProgram();
+            string a = GetRandomName();
+            string b = GetRandomName();
+            string c = GetRandomName();
+            string d = GetRandomName();
+            program.AddBlock(a, "fluid");
+            program.AddBlock(b, "mixer");
+            program.AddBlock(c, "getInput");
+            program.AddBlock(d, "getInput");
+            program.AddConnection(a, "inputFluid", b);
+            program.AddConnection(b, "inputFluidA", c);
+            program.AddConnection(b, "inputFluidB", d);
+        }
+
+        public string GetRandomName()
+        {
+            nameID++;
+            return "N" + nameID;
         }
 
         public override string ToString()
