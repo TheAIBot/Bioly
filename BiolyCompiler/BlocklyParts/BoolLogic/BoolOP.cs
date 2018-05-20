1f0883a5c3a460d53bf96085f0fea8d8d09e6a96
TheAIBot <andreasuranus@hotmail.com>
2018-05-20 10:35:45 +0200

    fixed if parsing test

--- BiolyCompiler/BlocklyParts/BoolLogic/BoolOP.cs
+++ BiolyCompiler/BlocklyParts/BoolLogic/BoolOP.cs
@@ -56,8 +56,8 @@ namespace BiolyCompiler.BlocklyParts.BoolLogic
             dfg.AddNode(rightBoolBlock);
 
             List<string> inputs = new List<string>();
-            inputs.Add(leftBoolBlock?.OriginalOutputVariable);
-            inputs.Add(rightBoolBlock?.OriginalOutputVariable);
+            inputs.Add(leftBoolBlock?.OutputVariable);
+            inputs.Add(rightBoolBlock?.OutputVariable);
 
             return new BoolOP(leftBoolBlock, rightBoolBlock, inputs, null, node, id, canBeScheduled);
         }
