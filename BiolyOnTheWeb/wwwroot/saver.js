7540c3911340eae049ae68cee329cad1acedab96
TheAIBot <andreasuranus@hotmail.com>
2019-08-23 05:46:42 +0200

    support quck save/load

---
+++ BiolyOnTheWeb/wwwroot/saver.js
@@ -0,0 +1,12 @@
+﻿const quickSaveLoadFileName = "__qsl__";
+
+function quickSave() {
+    window.localStorage[quickSaveLoadFileName] = getWorkspaceAsXml();
+}
+
+function quickLoad() {
+    const xmlString = window.localStorage[quickSaveLoadFileName];
+
+    Blockly.mainWorkspace.clear();
+    loadWorkspace(xmlString);
+}
